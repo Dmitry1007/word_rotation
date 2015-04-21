9d5cbec162bebc98444c73be9844f2bf62f05829
Dmitry Sky <dmitry.sky@icloud.com>
2015-04-21 11:21:13 -0600

    master refactor

--- word_rotation.rb
+++ word_rotation.rb
@@ -1,17 +1,5 @@
 class Wordrotate
-  def check(word1, word2)
-    if word1 == word2
-      true
-    elsif word1.chars.sort != word2.chars.sort
-      false
-    else
-      count = 0
-      until word1 == word2 || count == word1.size
-        word1 = word1.chars.rotate.join
-        rotated_word = word1
-        count += 1
-      end
-      rotated_word == word2
-    end
+  def check(word, compare_word)
+    (word + word).include?(compare_word) && compare_word.length == word.length
   end
 end
